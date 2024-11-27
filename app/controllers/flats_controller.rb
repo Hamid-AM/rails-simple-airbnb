0809cd52f024ff44dd6ea6ab44f68fcb190f05fa
abdelhamid ameziane <ameziane001@gmail.com>
2024-11-28 00:15:42 +0100

    simple airbnb exo

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,4 +4,26 @@ class FlatsController < ApplicationController
     @flats = Flat.all
   end
 
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(params_flat)
+    if @flat.save
+      redirect_to flats_path(@flat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  private
+
+  def params_flat
+    params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night)
+  end
 end
